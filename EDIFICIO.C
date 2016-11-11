af557fdcb55053a58d2bb9964a18f27c76186ab7
Jake Antonio Ruiz Zeledón <jackiezeledon@gmail.com>
2016-11-10 23:14:43 -0600

    Update EDIFICIO.C

--- EDIFICIO.C
+++ EDIFICIO.C
@@ -1,4 +1,8 @@
-
+/*Archivo principal que contiene
+las funciones y librerias
+Autor: Jake Zeledon 
+Universidad Nacional de Ingenieria (UNI) Managua, Nicaragua
+2015*/
 #include<conio.h>
 #include<stdio.h>
 #include<graphics.h>
